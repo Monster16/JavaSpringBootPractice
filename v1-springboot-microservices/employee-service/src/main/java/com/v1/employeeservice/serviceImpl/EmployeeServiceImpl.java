1b8630d04f98ffc60a71e51bd1fee46bcdbd19e0
Anil Singh <as.8108761450.as@gmail.com>
2023-11-25 17:09:27 +0530

    Udemy Course Video Section 12 Assignment 1
    Committing Udemy Course Video Section 12 Assignment 1

--- v1-springboot-microservices/employee-service/src/main/java/com/v1/employeeservice/serviceImpl/EmployeeServiceImpl.java
+++ v1-springboot-microservices/employee-service/src/main/java/com/v1/employeeservice/serviceImpl/EmployeeServiceImpl.java
@@ -2,9 +2,11 @@ package com.v1.employeeservice.serviceImpl;
 
 import com.v1.employeeservice.dto.EmployeeDto;
 import com.v1.employeeservice.entity.Employee;
+import com.v1.employeeservice.mapper.AutoEmployeeMapper;
 import com.v1.employeeservice.repo.EmployeeRepository;
 import com.v1.employeeservice.service.EmployeeService;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,22 +15,50 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     private EmployeeRepository employeeRepository;
 
+    private ModelMapper modelMapper;
+
     @Override
     public EmployeeDto createEmployee(EmployeeDto employeeDto) {
-        Employee employee = new Employee(
-                employeeDto.getId(),
-                employeeDto.getFirstName(),
-                employeeDto.getLastName(),
-                employeeDto.getEmail()
-        );
+        /**
+         * Directly Converting EmployeeDto to Employee JPA Entity
+         */
+//        Employee employee = new Employee(
+//                employeeDto.getId(),
+//                employeeDto.getFirstName(),
+//                employeeDto.getLastName(),
+//                employeeDto.getEmail()
+//        );
+        /**
+         * Using Model Mapper
+         */
+//        Employee employee = modelMapper.map(employeeDto, Employee.class);
+
+        /**
+         * Using Map Struct
+         */
+        Employee employee = AutoEmployeeMapper.AUTO_EMPLOYEE_MAPPER.mapToEmployee(employeeDto);
         Employee saveEmployee = employeeRepository.save(employee);
 
-        EmployeeDto saveEmployeeDto = new EmployeeDto(
-                saveEmployee.getId(),
-                saveEmployee.getFirstName(),
-                saveEmployee.getLastName(),
-                saveEmployee.getEmail()
-        );
+        /**
+         *   Directly Converting Employee JPA Entity to EmployeeDto
+         */
+//        EmployeeDto saveEmployeeDto = new EmployeeDto(
+//                saveEmployee.getId(),
+//                saveEmployee.getFirstName(),
+//                saveEmployee.getLastName(),
+//                saveEmployee.getEmail()
+//        );
+
+        /**
+         * Using Model Mapper
+         */
+//        EmployeeDto saveEmployeeDto = modelMapper.map(saveEmployee, EmployeeDto.class);
+
+        /**
+         * Using Map Struct
+         */
+        EmployeeDto saveEmployeeDto = AutoEmployeeMapper.AUTO_EMPLOYEE_MAPPER.mapToEmployeeDto(saveEmployee);
+
         return saveEmployeeDto;
     }
 
@@ -36,12 +66,24 @@ public class EmployeeServiceImpl implements EmployeeService {
     public EmployeeDto getEmployeeById(Long id) {
         Employee employee = employeeRepository.findById(id).get();
 
-        EmployeeDto employeeDto = new EmployeeDto(
-                employee.getId(),
-                employee.getFirstName(),
-                employee.getLastName(),
-                employee.getEmail()
-        );
+        /**
+         * Directly Converting EmployeeDto to Employee JPA Entity
+         */
+//        EmployeeDto employeeDto = new EmployeeDto(
+//                employee.getId(),
+//                employee.getFirstName(),
+//                employee.getLastName(),
+//                employee.getEmail()
+//        );
+        /**
+         * Using Model Mapper
+         */
+//        EmployeeDto employeeDto = modelMapper.map(employee, EmployeeDto.class);
+
+        /**
+         * Using Map Struct
+         */
+        EmployeeDto employeeDto = AutoEmployeeMapper.AUTO_EMPLOYEE_MAPPER.mapToEmployeeDto(employee);
         return employeeDto;
     }
 }
