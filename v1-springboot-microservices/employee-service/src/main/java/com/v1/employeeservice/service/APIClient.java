0d4311a479cde6ad732a8a76ae019cd8a5411e29
Anil Singh <as.8108761450.as@gmail.com>
2023-11-26 15:26:11 +0530

    Update APIClient.java
    Also adding commented line without load balance

--- v1-springboot-microservices/employee-service/src/main/java/com/v1/employeeservice/service/APIClient.java
+++ v1-springboot-microservices/employee-service/src/main/java/com/v1/employeeservice/service/APIClient.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "DEPARTMENT-SERVICE")
+//@FeignClient(url = "http://localhost:8083" ,value = "DEPARTMENT-SERVICE")
 public interface APIClient {
 
     @GetMapping("/api/departments/get/{department-code}")
